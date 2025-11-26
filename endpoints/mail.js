f9158c3dfd5b733f6957589df288c6e5f9436037
javierITB <vicente.torresb@mail.udp.cl>
2025-11-26 20:33:19 -0300

    correccion ruta mail

--- endpoints/mail.js
+++ endpoints/mail.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
-const { sendEmail } = require("./mail.helper"); // Importamos la lógica
+const { sendEmail } = require("../utils/mail.helper"); // Importamos la lógica
 
 const router = express.Router();
 
